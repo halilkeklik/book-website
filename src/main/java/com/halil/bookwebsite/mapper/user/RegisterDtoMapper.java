047f12833fbe47e5a169b3bbe96b352340c626cb
halilkeklik <halil@halilkeklik.com>
2024-06-06 13:37:55 +0300

    Update autowired RegisterDtoMapper

--- src/main/java/com/halil/bookwebsite/mapper/user/RegisterDtoMapper.java
+++ src/main/java/com/halil/bookwebsite/mapper/user/RegisterDtoMapper.java
@@ -3,7 +3,10 @@ package com.halil.bookwebsite.mapper.user;
 import com.halil.bookwebsite.dtos.UserRegisterDto;
 import com.halil.bookwebsite.entities.User;
 import com.halil.bookwebsite.mapper.Mapper;
+import jdk.jfr.Category;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RegisterDtoMapper extends Mapper<User, UserRegisterDto> {
     @Override
     public UserRegisterDto maptoB(User user) {
